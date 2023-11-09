60b89d5c4e86bab2db35cd3b907039077ade1b29
maedayukimi <maedayukimi@icloud.com>
2023-11-09 10:06:19 +0900

    post data->view

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController; 
 
 
-Route::get('/posts', [PostController::class, 'index']);
-
+Route::get('/', [PostController::class, 'index']);
