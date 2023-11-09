143743df38cdfa60f60c9c2f3e501069ee08604a
maedayukimi <maedayukimi@icloud.com>
2023-11-09 16:27:02 +0900

    mariadb->view, and succeed 10 limits to show

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,28 +1,15 @@
 <?php
-
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+    public function boot(){
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
+
+        Paginator::useBootstrapFive();
+       //または Paginator::useBootstrapFour();　　   公式ドキュメント
     }
 }
