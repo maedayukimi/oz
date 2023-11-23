a44c429bc0594de9c5b969b473e54bda42f07bbd
maedayukimi <maedayukimi@icloud.com>
2023-11-23 16:17:22 +0900

    8-4 create

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -11,6 +11,7 @@
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
+                    <a href='/posts/create'>create</a>
                     <h2 class='title'><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h2>
                     <p class='body'>{{ $post->body }}</p>
                 </div>
