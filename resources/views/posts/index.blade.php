e110cb283fd43be940b3ab3cdb5a73014ec53839
maedayukimi <maedayukimi@icloud.com>
2023-12-15 16:41:20 +0900

    8-6 finish

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,15 +9,31 @@
     <body>
         <h1>Blog Name</h1>
         <div class='posts'>
+            <a href='/posts/create'>create</a>
+
             @foreach ($posts as $post)
                 <div class='post'>
-                    <a href='/posts/create'>create</a>
                     <h2 class='title'><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h2>
                     <p class='body'>{{ $post->body }}</p>
+                    
+                    <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button type="button" onclick="deletePost({{ $post->id }})">delete</button> 
+                    </form>
                 </div>
             @endforeach
         </div>
          <div class='paginate'>
             {{ $posts->links() }}
+            <script>
+                function deletePost(id) {
+                    'use strict'
+    
+                if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+                    document.getElementById(`form_${id}`).submit();
+                }
+            }
+        </script>
     </body>
 </html>
